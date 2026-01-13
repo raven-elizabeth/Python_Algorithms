91d03535a6897686859de5dc7729ee9d913ae48f
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2026-01-13 10:09:40 +0000

    find_shortest_distances implemented & passing unit test

--- Path_Planning/Dijkstra/Graph.py
+++ Path_Planning/Dijkstra/Graph.py
@@ -1,3 +1,5 @@
+from heapq import heapify, heappop, heappush
+
 class Graph:
     def __init__(self, graph):
         self.graph = graph
@@ -10,3 +12,44 @@ class Graph:
         self.graph[node1][node2] = weight
         self.graph[node2][node1] = weight
 
+    # Returns dictionary containing each node with its value being the shortest possible distance to it from source node
+    def find_shortest_distances(self, source_node):
+        if source_node not in self.graph:
+            return None
+
+        # Create dictionary to track distance from start node
+        # Set all to infinity initially as currently unknown
+        distances_from_source_node = {node: float("inf") for node in self.graph}
+        # Initialize root node as 0 (already at start)
+        distances_from_source_node[source_node] = 0
+
+        # Create priority queue to sort distance values (heapify converts tuple list to pq)
+        priority_queue = [(0, source_node)]
+        heapify(priority_queue)
+
+        ## Track already visited nodes with a set
+        visited_nodes = set()
+
+        ## Use while loop to search neighbouring nodes
+        # while pq is not empty...
+        while priority_queue:
+            # heappop finds min distance and removes from pq
+            curr_distance, curr_node = heappop(priority_queue)
+
+            # Track visited nodes
+            if curr_node not in visited_nodes:
+                visited_nodes.add(curr_node)
+
+            # Search neighbouring nodes to find smallest distance
+            for neighbour, weight in self.graph[curr_node].items():
+                # Set current distance from start
+                new_distance_from_node1 = curr_distance + weight
+
+                # Check if new distance is shorter than what is already recorded for this neighbour
+                if new_distance_from_node1 < distances_from_source_node[neighbour]:
+                    # If so, replace old distance to recognise shorter possible route
+                    distances_from_source_node[neighbour] = new_distance_from_node1
+                    # Then re-add this node to the pq so this new possible route can be checked
+                    heappush(priority_queue, (new_distance_from_node1, neighbour))
+
+        return distances_from_source_node
