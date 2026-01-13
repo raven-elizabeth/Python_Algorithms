72b74048591cae3ea95e5c7171798986d50ccc3d
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2026-01-13 10:07:11 +0000

    Add edge method created

--- Path_Planning/Dijkstra/Graph.py
+++ Path_Planning/Dijkstra/Graph.py
@@ -1,3 +1,12 @@
 class Graph:
     def __init__(self, graph):
         self.graph = graph
+
+    def add_edge(self, node1, node2, weight):
+        if node1 not in self.graph:
+            self.graph[node1] = {}
+        if node2 not in self.graph:
+            self.graph[node2] = {}
+        self.graph[node1][node2] = weight
+        self.graph[node2][node1] = weight
+
